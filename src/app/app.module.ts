a9148b62dadafcecb5ad783660fc0a2fd22c6e0b
alan skaev <alan.skaev22@gmail.com>
2021-05-09 21:31:49 -0400

    contact page + angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { AppComponent } from './app.component';
 import { BrowsePartsComponent } from './browse-parts/browse-parts.component';
 import { HomeComponent } from './home/home.component';
@@ -9,6 +12,8 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -22,7 +27,12 @@ import { ContactComponent } from './contact/contact.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,15 +1,58 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
+
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  contactForm: FormGroup = new FormGroup({});
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.contactForm = this.fb.group({
+      firstName: ['', [
+        Validators.required,
+        Validators.maxLength(15)
+      ]],
+      lastName: ['', [
+        Validators.required,
+        Validators.maxLength(15)
+      ]],
+      email: ['', [
+        Validators.required,
+        Validators.email
+      ]],
+      comment: ['', [
+        Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(25)
+      ]]
+    });
+  }
+
+  get firstName() {
+    return this.contactForm.get('firstName');
+  }
+
+  get lastName() {
+    return this.contactForm.get('lastName');
+  }
+
+  get comment() {
+    return this.contactForm.get('comment');
+  }
+
+  get email() {
+    return this.contactForm.get('email');
+  }
+  onSubmit(){
+    console.log(this.contactForm);
   }
 
 }
