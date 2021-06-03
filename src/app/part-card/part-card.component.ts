fa896763d876a0411c153e082b774d16fb17acbd
alan skaev <alan.skaev22@gmail.com>
2021-06-02 20:23:17 -0400

    styling

--- src/app/part-card/part-card.component.ts
+++ src/app/part-card/part-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-part-card',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./part-card.component.css']
 })
 export class PartCardComponent implements OnInit {
-
+  @Input() partId:any;
   constructor() { }
 
   ngOnInit(): void {
